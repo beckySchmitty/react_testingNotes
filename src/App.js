bfa712bcf8dde620b02b236f703b254a2d8eefc8
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-02-26 13:20:52 -0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, I'm an app!</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
