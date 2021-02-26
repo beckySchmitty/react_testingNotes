3c9c3c0612d6cb3672a91e2958b0a91202dd8fd1
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-02-26 13:26:32 -0500

    smoke test notes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// smoke test
+test('renders without crashing', function () {
+  render(<App />)
 });
